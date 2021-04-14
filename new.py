e2319fd8e074788394b7b864d3f534483f7150de
azim <az1234im@gmail.com>
2021-04-14 14:08:02 +0530

    __dict__(Fordoc) of class

--- new.py
+++ new.py
@@ -1,1 +1,13 @@
-print("hello")
+class Employee:
+    
+    def __init__(self):
+        self.eno=1
+        self.ename='Azim'
+        self.eage=21
+        self.esal="NIL"
+
+
+e=Employee()
+
+
+print(e.__dict__)    
