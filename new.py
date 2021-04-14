c92e63f24610ab96981d052c352c7df87a2ee3d9
azim <az1234im@gmail.com>
2021-04-14 14:16:14 +0530

    creatinfmultiple obeject(parameters->self)

--- new.py
+++ new.py
@@ -11,3 +11,19 @@ e=Employee()
 
 
 print(e.__dict__)    
+
+class Student:
+    ''' this is student class '''
+    def __init__(self,x,y,z):
+        self.name=x
+        self.age=y
+        self.rollno=z
+
+    def display(self):
+        print("Student Name: {} \n Student Age : {} \n Student rollno: {}".format(self.name,self.age,self.rollno))
+
+s1=Student('Azim',21,'1pse17cs55')
+s2=Student('Hari',21,'1pse17cs89')
+
+s1.display()
+s2.display()
