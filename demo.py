78d3bcb31f3786054215395cc59c9d211a101ea7
azim <az1234im@gmail.com>
2021-04-20 16:56:00 +0530

    program to track no of objects

--- demo.py
+++ demo.py
@@ -6,3 +6,26 @@ class Animal:
 
 Animal.walk('Dog')        
 Animal.walk('cat')
+
+#  Program to track the number of objects created for a class
+
+class Test:
+    count=0 #this is class variable 
+    def __init__(self):
+        Test.count=Test.count+1
+
+    @classmethod
+    def noOfObject(cls):
+        print("The number of objects CReated are for the class ",cls.count)
+
+
+t1=Test()
+t2=Test()
+
+Test.noOfObject()
+
+t3=Test()
+t4=Test()
+t5=Test()
+
+Test.noOfObject()
