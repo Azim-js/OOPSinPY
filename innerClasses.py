0ec0493af5e6d5ca2416440c3b833825df5d3156
azim <az1234im@gmail.com>
2021-04-24 23:32:47 +0530

    exapmle 1

--- innerClasses.py
+++ innerClasses.py
@@ -12,3 +12,26 @@ class Outer:
 o=Outer()
 i=o.Inner() #inner class object creation using outer creation
 i.m1() #calling the inner class            
+
+
+class Person:
+    def __init__(self):
+        self.name='Azim'
+        self.db=self.Dob()
+
+    def display(self):
+        print("NAme: ",self.name)    
+
+    class Dob:
+        def __init__(self):
+            self.dd=31
+            self.mm=5
+            self.yy=1999    
+
+        def display(self):
+            print('Dob={}/{}/{}'.format(self.dd,self.mm,self.yy))    
+
+p=Person()
+p.display()
+x=p.db
+x.display()       
